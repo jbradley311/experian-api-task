e2f5ef896c83a3a3c00632f1b9704d71b4ee1362
jbradley311 <jbradley311@gmail.com>
2022-02-12 18:37:36 +0000

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# experian-api-task
---
+++ src/main/java/com/example/company/message/MessageApplication.java
@@ -0,0 +1,13 @@
+package com.example.company.message;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MessageApplication.class, args);
+	}
+
+}
